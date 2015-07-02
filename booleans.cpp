b2a0c7ee263343a8b1d2ab4d18c275941c22ff98
Ibtissam <ik@karouach.se>
2015-07-02 14:10:32 +0200

    Boolean Types & Use

---
+++ booleans.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+using namespace std;
+
+int main() {
+
+    bool aBooleanVariable;
+    aBooleanVariable = true;
+
+    printf("The Boolean Value is: %d\n", aBooleanVariable);
+
+
+    return EXIT_SUCCESS;
+}
