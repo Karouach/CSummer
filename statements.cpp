f70b39b22ef9f130d5fff473cc54538b9379b474
Ibtissam <ik@karouach.se>
2015-07-02 13:52:45 +0200

    Statements & Expressions

---
+++ statements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+
+    int days = 12 + 5 + 4;
+    double width = 89.07;
+    double height = 34.9;
+    double perimeter = width + width +  height + height;
+
+    printf("Days: %d \n", days);
+    printf("Perimeter: %f \n", perimeter );
+
+
+    return EXIT_SUCCESS;
+}
