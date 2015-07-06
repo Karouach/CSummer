31580ce75017ef616b51eaae168775d0e12fb791
Ibtissam <ik@karouach.se>
2015-07-06 09:34:15 +0200

    Data Management & Random Access Files

--- files.c
+++ files.c
@@ -2,52 +2,43 @@
  ============================================================================
  Name        : files.c
  Author      :
- Description : Sequential file read and write, Ansi-style
+ Description : Data management using random access files, Ansi-style
  ============================================================================
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-void readEntireFile(void);
-void readUntillMarker(char marker);
-void writeToFile(void);
 
-int main(void){
- //readEntireFile();
- //readUntilMarker("m");
-  writeToFile();
+
+void copyFromSourceToDestination(void);
+void appendCopyrightNotice(void);
+
+int main(void) {
+
+ copyFromSourceToDestination();
+ //appendCopyrightNotice();
 
  return EXIT_SUCCESS;
+}
 
+void copyFromSourceToDestination(){
+   int ch;
+   FILE *src, *dest;
+   src = fopen("path","r");
+   dest = fopen("path","w");
+   ch = getc(src);
+     while (ch != EOF){
+       putc(ch, dest);
+       ch = getc(src);
+     }
+   fclose(src);
+   fclose(dest);
 }
-void readEntireFile(){
- int ch;
- FILE *fp;
- fp = fopen("C:/Users/ClionProjects/Csummer2/message", "r");
- ch = getc(fp);
- while (ch != EOF){
-  putchar(ch);
-  ch = getc(fp);
- }
- fclose(fp);
 
-}
-void readUntilMarker(char marker){
- int ch;
- FILE *fp;
- fp = fopen("C:/Users/ClionProjects/Csummer2/message", "r"); // Change to match your path
- ch = getc(fp);
- while (ch != marker){
-  putchar(ch);
-  ch = getc(fp);
- }
- fclose(fp);
-}
-void writeToFile(){
- FILE *fp;
- fp = fopen("C:/Users/ClionProjects/Csummer2/message2", "w");
- char name[10] = "Hello";
- fprintf(fp, "This is a message foe %s\n", name);
- fclose(fp);
+void appendCopyrightNotice(){
+    FILE *fp;
+    fp = fopen("path", "a"); // Change to match your path
+    fprintf(fp,"\nCopyright 2015, test");
+    fclose(fp);
 }
