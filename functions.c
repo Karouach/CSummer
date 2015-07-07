73f011507d001822d2e1ba1a09077858fc035564
Ibtissam <ik@karouach.se>
2015-07-07 13:15:08 +0200

    Example Template Functions in C

---
+++ functions.c
@@ -0,0 +1,57 @@
+/*
+ ============================================================================
+ Name        : functions.c
+ Author      :
+ Description : User defined functions, Ansi-style (Example Template)
+ ============================================================================
+ */
+
+#include <stdlib.h>
+
+
+int main(void) {
+
+     readData();
+     validateData();
+     processData();
+     analyzeResults();
+     generateReport();
+     writeFormattedReport();
+     archiveData();
+     disseminateResults();
+
+ return EXIT_SUCCESS;
+}
+
+//---- FUNCTION DEFINITIONS ----
+
+void readData(){
+ //Enter function code here
+}
+
+void validateData(){
+ //Enter function code here
+}
+
+void processData(){
+ //Enter function code here
+}
+void analyzeResults(){
+ //Enter function code here
+}
+
+void generateReport(){
+ //Enter function code here
+}
+
+void writeFormattedReport(){
+ //Enter function code here
+}
+
+void archiveData(){
+ //Enter function code here
+}
+
+void disseminateResults(){
+ //Enter function code here
+}
