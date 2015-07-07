17bc904d96c94e38f9ce2724d8ad272c9e5b5d77
Ibtissam <ik@karouach.se>
2015-07-07 14:25:40 +0200

    Passing Pointer to Function

--- arraypointertofunc.c
+++ arraypointertofunc.c
@@ -1,25 +1,25 @@
 /*
  ============================================================================
- Name        : arraytofunc.c
- Description : Passing an array to a function, Ansi-style
+ Name        : arraypointertofunc.c
+ Description : Passing an array (Pointer) to a function, Ansi-style
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void func1(int array[]);
+void func2(int *array);
 
 
 int main(void) {
 
  int list[5] = {1, 2, 3, 4, 5};
- func1(list);
+ func2(list);
 
  return EXIT_SUCCESS;
 }
 
-void func1(int array[]){
+void func2(int *array){
  printf("Array[3] = %d\n", array[3]);
 }
 
